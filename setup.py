2c019edf689bfadda72ffbba163126d301e57a83
Yishun <yishun.lu@eng.ox.ac.uk>
2023-01-10 11:20:13 +0000

    added yaml for conda building and replace  install_requires in setup.py with requirementsin the yaml file

--- setup.py
+++ setup.py
@@ -6,15 +6,7 @@ setup(
     description='AnACor for analytical absorption correction by tomography reconstruction',
     author='Yishun Lu',
     author_email='yishun.lu@eng.ox.ac.uk, wes.armour@oerc.ox.ac.uk',
-    
-    install_requires=[
-        'importlib-metadata; python_version == "3.8"',
-        'opencv-python',
-        'scikit-image',
-        'numba',
-        'imagecodecs'
 
-    ],
 
     entry_points = {
         'console_scripts' :
@@ -22,3 +14,12 @@ setup(
              'anacor.main = AnACor.main:main',],
     }
 )
+#    install_requires=[
+#        'importlib-metadata; python_version == "3.8"',
+#        'opencv-python',
+#        'scikit-image',
+#        'numba',
+#        'imagecodecs',
+#        'dials @ https://github.com/yishunlu-222/dials_precalcu/tree/precalc_abs_model',
+#
+#    ],
